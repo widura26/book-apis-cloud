357063d6592fc8c159c965485799167e2e69c27b
widura26 <widurategalrejo@gmail.com>
2023-12-07 20:40:43 +0700

    haduh

--- samples/firestoreClient.js
+++ samples/firestoreClient.js
@@ -1,7 +1,7 @@
 import { Firestore } from "@google-cloud/firestore";
 const firestore = new Firestore({
     projectId: "trial-project-406012",
-    keyFilename: 'D:\\project\\book-apis-cloud\\samples\\keyfile.json'
+    keyFilename: './keyfile.json'
 });
 
 export default firestore;
