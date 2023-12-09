5b6e0625707ac5f7ff68451d039b28c91241c42f
widura26 <widurategalrejo@gmail.com>
2023-12-09 18:54:04 +0700

    commit

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -31,7 +31,6 @@ class BookController {
                     // resolve();
                 }).end();
             // })
-            console.log('file berhasil diupload');
         } catch (error) {
             console.log(error);
         }
