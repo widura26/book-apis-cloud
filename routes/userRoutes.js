349c2531554e1e52e7281226241c56e3154adf68
widura26 <widurategalrejo@gmail.com>
2023-12-17 19:14:45 +0700

    commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import UserController from '../controllers/UserController.js';
 const router = express.Router();
 
-router.post('/signin', new UserController().signup);
+router.post('/signin', new UserController().signin);
 router.post('/login', new UserController().login);
 
 export default router;
